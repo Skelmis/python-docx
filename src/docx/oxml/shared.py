e88a18ba719f5eb8a275084c2ecb6e1f53faeb31
skelmis <skelmis.craft@gmail.com>
2024-09-02 20:08:53 +1200

    feat: merge in python-docx PR 582

--- src/docx/oxml/shared.py
+++ src/docx/oxml/shared.py
@@ -6,8 +6,17 @@ from typing import cast
 
 from docx.oxml.ns import qn
 from docx.oxml.parser import OxmlElement
-from docx.oxml.simpletypes import ST_DecimalNumber, ST_OnOff, ST_String
+from docx.oxml.simpletypes import ST_DecimalNumber, ST_OnOff, ST_String, ST_LongHexNumber
 from docx.oxml.xmlchemy import BaseOxmlElement, OptionalAttribute, RequiredAttribute
+
+
+class CT_LongHexNumber(BaseOxmlElement):
+    """
+    ``<w:suff>`` element, which specifies the form of the space
+    between a list number and the list paragraph
+    """
+
+    val = RequiredAttribute("w:val", ST_LongHexNumber)
 
 
 class CT_DecimalNumber(BaseOxmlElement):
